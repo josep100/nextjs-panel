40c02a487241b3c1595ae7202f2815e3eebe9fd2
josep100 <josep_1000@hotmail.com>
2025-03-23 16:36:31 +0100

    Add files via upload
    Versión final del panel de facturas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+      template: '%s | Panel de control de Acme',
+      default: 'Panel de control de Acme',
+    },
+    description: 'El panel de control oficial del curso de Next.js, creado con App Router',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -14,9 +14,9 @@ export default async function CustomersTable({
   return (
     <div className="w-full">
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
-        Customers
+        Clientes
       </h1>
-      <Search placeholder="Search customers..." />
+      <Search placeholder="Buscar clientes..." />
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
@@ -66,19 +66,19 @@ export default async function CustomersTable({
                 <thead className="rounded-md bg-gray-50 text-left text-sm font-normal">
                   <tr>
                     <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                      Name
+                      Nombre
                     </th>
                     <th scope="col" className="px-3 py-5 font-medium">
                       Email
                     </th>
                     <th scope="col" className="px-3 py-5 font-medium">
-                      Total Invoices
+                      Facturas totales
                     </th>
                     <th scope="col" className="px-3 py-5 font-medium">
-                      Total Pending
+                      Pendiente Total 
                     </th>
                     <th scope="col" className="px-4 py-5 font-medium">
-                      Total Paid
+                      Pagado total
                     </th>
                   </tr>
                 </thead>
