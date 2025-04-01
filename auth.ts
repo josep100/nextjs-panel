903b687ee157b229c8ba9f3e21bf4cac1616c220
josep100 <josep_1000@hotmail.com>
2025-04-01 21:48:24 +0200

    Add files via upload

--- auth.ts
+++ auth.ts
@@ -16,8 +16,9 @@ async function getUser(email: string): Promise<User | undefined> {
   }
 }
  
-export const { auth, signIn, signOut } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  trustHost: true,
   providers: [
     Credentials({
       async authorize(credentials) {
